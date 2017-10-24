8d13df38c4e90b13a763a72740166469dd5968ff
vikas <vikas@vikass-iMac.local>
2017-10-24 11:19:19 +0530

    Initial Commit

---
+++ VideoARDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoARDemo
+//
+//  Created by vikas on 24/10/17.
+//  Copyright © 2017 Artiligence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
